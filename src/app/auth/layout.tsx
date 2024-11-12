af95199bfa82328a046d13570573b3f1e0747b45
Samuel Chibueze <52251326+samc1415@users.noreply.github.com>
2024-11-12 07:38:28 -0600

    Update layout.tsx

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -6,11 +6,8 @@ export default function AuthLayout({
   children: React.ReactNode
 }) {
   return (
-HEAD
     <div className="w-full lg:grid lg:grid-cols-3 min-h-[calc(100vh-52px)]">
-    <div className="w-full lg:grid lg:grid-cols-3 min-h-screen">
-ed53fb7 (Updated files)
       {children}
     </div>
-    )
+  );
 }
